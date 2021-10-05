e8a57162f031d4043ed7b62a07748251c2ea7cc6
nelson10 <38512291+nelson10@users.noreply.github.com>
2021-10-05 17:49:43 -0500

    Add files via upload

--- Main2.m
+++ Main2.m
@@ -9,7 +9,7 @@ addpath(genpath('./dataset1'));
 addpath(genpath('./src'));
 
 %% load point cloud points
-data = 2;
+data = 3;
 if(data ==1 )
     dataset = "ajaccio_2";
 elseif(data ==2)
@@ -36,53 +36,10 @@ x = standart(x);
 y = standart(y);
 z = standart(z);
 
-X = [x y z];
-temp = X;
+X = [x y z]; % Cloud point
 
 indices = knnsearch(X,X,'K',50,'Distance','euclidean');
 
 [ind] = GroundExtraction2(N,X,indices,ptCloud,dataset);
 [ind2] = naivesClassifier(N,indices,ind,ptCloud,K);
-ind = ind2;
-p = 0;
-x = x(ind==p);
-y = y(ind==p);
-z = z(ind==p);
-M = length(z);
-
-indg = (ind==p);
-arr = X(indg,:);
-
-A =[x y ones(M,1)];
-C = inv(A'*A);
-B = C*A'*z;
-close all;
-
-[X,Y] = meshgrid(linspace(min(x),max(x),50), linspace(min(y),max(y),50));
-Z = B(1)*X + B(2)*Y + B(3)*ones(size(X));
-figure(1)
-plot3(temp(indg==1,1),temp(indg==1,2),temp(indg==1,3),'.')
-hold on
-meshc(X, Y, Z)
-hold off
-grid on
-xlabel('x(mm)'); ylabel('y(mm)'); zlabel('z(mm)');
-title('Masked plot');
-grid on
-%text(-20, 50, 450, sprintf('Z = %.3f\\cdotX %+.3f\\cdotY %+3.0f', B))
-
-d = (B(1)*x + B(2)*y + z +B(3))/(sqrt(sum(B)));
-m = mean(d);
-de = std(d);
-gr = (d >= m-de) & (d <= m+de);
-figure(2)
-plot3(temp(:,1),temp(:,2),temp(:,3),'b.')
-hold on
-%meshc(X, Y, Z)
-%hold on
-plot3(arr(gr,1),arr(gr,2),arr(gr,3),'r.')
-%plot3(temp(indg==1,1),temp(indg==1,2),temp(indg==1,3),'r.')
-%meshc(X, Y, Z)
-%[U,V] = results2(ptCloud,gr,N)
-
-
+fit2plane(X,x,y,z,ind2);
